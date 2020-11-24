9e0307bf6de0bea233e7aa839fa43ce9f8a24106
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2020-11-24 20:12:55 +0100

    Fixed school

--- src/src/controllers/SchoolController.java
+++ src/src/controllers/SchoolController.java
@@ -15,28 +15,28 @@ public class SchoolController extends FXController {
 
     @Override
     public void update() {
-//        Bounds playerBounds = getMain().getView().lookup("#player").getBoundsInParent();
-//        Bounds exitBounds = getMain().getView().lookup("#exit").getBoundsInParent();
-//
-//        Label help = (Label) getMain().getView().lookup("#help");
-//
-//        if(playerBounds.intersects(exitBounds)) {
-//            helpMessage("Press 'F' to exit.", help);
-//        } else {
-//            help.setVisible(false);
-//        }
+        Bounds playerBounds = getMain().getView().lookup("#player").getBoundsInParent();
+        Bounds exitBounds = getMain().getView().lookup("#exit").getBoundsInParent();
+
+        Label help = (Label) getMain().getView().lookup("#help");
+
+        if(playerBounds.intersects(exitBounds)) {
+            helpMessage("Press 'F' to exit.", help);
+        } else {
+            help.setVisible(false);
+        }
     }
 
 
     @Override
     public void onKeyPressed(KeyCode keyCode) {
-//        Bounds playerBounds = getMain().getView().lookup("#player").getBoundsInParent();
-//        Bounds switchSceneBounds = getMain().getView().lookup("#exit").getBoundsInParent();
-//
-//        if(keyCode == KeyCode.F && playerBounds.intersects(switchSceneBounds)) {
-//            getMain().setView("map");
-//            getMain().getCharacter().setX((int) getMain().getView().lookup("#school").getLayoutX());
-//            getMain().getCharacter().setY((int) getMain().getView().lookup("#school").getLayoutY());
-//        }
+        Bounds playerBounds = getMain().getView().lookup("#player").getBoundsInParent();
+        Bounds switchSceneBounds = getMain().getView().lookup("#exit").getBoundsInParent();
+
+        if(keyCode == KeyCode.F && playerBounds.intersects(switchSceneBounds)) {
+            getMain().setView("map");
+            getMain().getCharacter().setX((int) getMain().getView().lookup("#school").getLayoutX());
+            getMain().getCharacter().setY((int) getMain().getView().lookup("#school").getLayoutY());
+        }
     }
 }
