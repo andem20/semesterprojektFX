47a90e4bf4b53a06688f896579de54ba7d121648
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2020-12-10 20:46:07 +0100

    Created cobblestone.png

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module semesterprojektFX {
-  requires java.desktop;
-  requires javafx.base;
-  requires javafx.controls;
-  requires javafx.fxml;
-  opens src.presentation to javafx.fxml;
-  opens src.presentation.controllers to javafx.fxml;
-  exports src;
-}
